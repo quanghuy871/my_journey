75a2ec39c26aadeee24ec08478d919a506fad3c1
quanghuy871 <quanghuy871@gmail.com>
2021-06-03 18:49:36 +0700

    Final

---
+++ wordtyping_app/app.js
@@ -0,0 +1,19 @@
+let test = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
+  'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, ' +
+  'when an unknown printer took a galley of type and scrambled it to make a type specimen book. ' +
+  'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
+  'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
+  'and more recently with desktop publishing software like Aldus PageMaker ' +
+  'including versions of Lorem Ipsum.';
+
+let i = 0;
+
+const effect = function () {
+  if (i < test.length) {
+    document.querySelector('.demo').textContent += test[i];
+    i++;
+    setTimeout(effect, 10);
+  }
+}
+
+effect();
