8e4176fd9d7ab691a2274c2aa1403c157a187554
quanghuy871 <quanghuy871@gmail.com>
2021-08-18 11:26:28 +0700

    NEW quiz app

--- quiz_app/app.js
+++ quiz_app/app.js
@@ -22,6 +22,7 @@ class App {
   _quizLocation = document.querySelector('.quiz');
   _submit = document.querySelector('.submit');
   _next = document.querySelector('.next');
+  _questionDisplay = {};
   _question = 0;
 
   constructor() {
@@ -31,10 +32,35 @@ class App {
   event() {
     this.addQuiz();
     this.nextQuiz();
+    this.submitQuiz();
   }
 
   addQuiz() {
+    this._questionDisplay = DUMMY_QUIZ[this._question];
+    const markup = `
+    <div>
+      <h4 style="margin-bottom: 0">${this._questionDisplay.question}</h4>
+      <input class="check-box" type="checkbox" id="quesA" value="${this._questionDisplay.options.A}">
+      <label for="quesA">${this._questionDisplay.options.A}</label><br>
+      
+      <input class="check-box" type="checkbox" id="quesB" value="${this._questionDisplay.options.B}">
+      <label for="quesB">${this._questionDisplay.options.B}</label>
+    </div>
+    `;
 
+    this._quizLocation.innerHTML = '';
+    document.querySelector('.results').textContent = '';
+    this._quizLocation.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  answer() {
+    let answer = '';
+    document.querySelectorAll('.check-box').forEach(el => {
+      if (el.checked === true) {
+        answer = el.value;
+      }
+    })
+    return answer;
   }
 
   nextQuiz() {
@@ -42,12 +68,25 @@ class App {
       e.preventDefault();
       if (this._question + 1 < DUMMY_QUIZ.length) {
         this._question++;
+        this.addQuiz();
       }
     }.bind(this))
   }
 
   submitQuiz() {
+    this._submit.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelectorAll('.check-box').forEach(el => el.disabled = true);
+      this.checkAnswer();
+    }.bind(this))
+  }
 
+  checkAnswer() {
+    if (this.answer() === this._questionDisplay.answer) {
+      document.querySelector('.results').textContent = 'CORRECT';
+    } else {
+      document.querySelector('.results').textContent = 'INCORRECT';
+    }
   }
 }
 
